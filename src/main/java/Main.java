c73cee86a14b5ac7ce215786f276ec0d81347cc1
Federico Terzi <freddy6896@gmail.com>
2017-11-10 00:14:51 +0100

    First keyboard commits

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import net.model.KeyboardKeys;
 import net.packets.KeyboardShortcutPacket;
 import window.Window;
 import window.WindowManager;
@@ -5,8 +6,10 @@ import window.WindowManagerFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.List;
 
 public class Main {
@@ -16,7 +19,18 @@ public class Main {
         System.out.println(wm.getActivePID());
         List<Window> list = wm.getWindowList();
 
-        System.out.println(new KeyboardShortcutPacket("ctrl+c"));
+        Field[] fields = java.awt.event.KeyEvent.class.getDeclaredFields();
+        for (Field f : fields) {
+            if (Modifier.isStatic(f.getModifiers())) {
+                try {
+                    Integer i = f.getInt(java.awt.event.KeyEvent.class);
+                    System.out.println(f.getName() + " (\""+f.getName()+"\", \""+f.getName()+"\", " +i+"),");
+                } catch (IllegalAccessException e) {
+                    //e.printStackTrace();
+                }
+
+            }
+        }
 
         System.exit(0);
     }
--- src/main/java/net/packets/KeyboardShortcutPacket.java
+++ src/main/java/net/packets/KeyboardShortcutPacket.java
@@ -1,5 +1,7 @@
 package net.packets;
 
+import java.util.StringTokenizer;
+
 /**
  * Represent a keyboard shortcut event.
  */
@@ -7,7 +9,7 @@ public class KeyboardShortcutPacket extends DEPacket {
 
     public static final int OP_TYPE = 1001;
 
-    public KeyboardShortcutPacket(String keyCombination) {
+    public KeyboardShortcutPacket(String keyCombination) throws KeyboardShortcutParseException {
         super(parseKeyCombination(keyCombination));
         this.setOpType(OP_TYPE);
     }
@@ -17,7 +19,24 @@ public class KeyboardShortcutPacket extends DEPacket {
      * @param keyCombination the keyboard combination to parse
      * @return the validated keyboard combination string
      */
-    private static String parseKeyCombination(String keyCombination) {
+    private static String parseKeyCombination(String keyCombination) throws KeyboardShortcutParseException{
+        // Remove spaces and trim
+        keyCombination = keyCombination.trim().replace(" ", "");
+
+        // Analyze the string with a tokenizer
+        StringTokenizer st = new StringTokenizer(keyCombination, "+");
+
+        // TODO
+
         return null;
+    }
+
+    /**
+     * Thrown when parsing a wrong keyboard shortcut.
+     */
+    class KeyboardShortcutParseException extends Exception {
+        public KeyboardShortcutParseException(String message) {
+            super(message);
+        }
     }
 }
