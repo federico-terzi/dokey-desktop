bd639dcbbf8e42d19dd76b8557a2c53ea44dfb12
Federico Terzi <freddy6896@gmail.com>
2018-08-31 15:25:44 +0200

    First draft of tab selector

--- src/main/java/app/control_panel/tab_selector/Tab.kt
+++ src/main/java/app/control_panel/tab_selector/Tab.kt
@@ -1,24 +1,46 @@
 package app.control_panel.tab_selector
 
+import javafx.geometry.Pos
 import javafx.scene.control.Button
-import javafx.scene.control.ContentDisplay
 import javafx.scene.control.Label
 import javafx.scene.image.ImageView
 import javafx.scene.layout.VBox
 import system.image.ImageResolver
 
 class Tab(val imageResolver: ImageResolver, val tabLabel : String, val tabImage : String) : Button() {
+    private var nameLabel : Label
+
+    private var _selected = false
+    var selected : Boolean
+        get() = _selected
+        set(value) {
+            if (value) {
+                styleClass.add("tab-selected")
+
+                nameLabel.text = tabLabel
+            }else{
+                styleClass.clear()
+                styleClass.add("tab-selector-tab")
+
+                nameLabel.text = ""
+            }
+
+            _selected = value
+        }
+
     init {
+        styleClass.add("tab-selector-tab")
+
         val vBox = VBox()
-        val label = Label(tabLabel)
+        vBox.alignment = Pos.CENTER
+        nameLabel = Label(tabLabel)
         val image = imageResolver.resolveImage(tabImage, 24)
         val imageView = ImageView(image)
         imageView.fitHeight = 24.0
         imageView.fitWidth = 24.0
 
-        vBox.children.addAll(imageView, label)
+        vBox.children.addAll(imageView, nameLabel)
 
         graphic = vBox
-        contentDisplay = ContentDisplay.TOP
     }
 }
--- src/main/java/app/control_panel/tab_selector/TabSelector.kt
+++ src/main/java/app/control_panel/tab_selector/TabSelector.kt
@@ -1,32 +1,64 @@
 package app.control_panel.tab_selector
 
+import javafx.application.Platform
+import javafx.scene.Group
 import javafx.scene.layout.Pane
+import javafx.scene.shape.Rectangle
+import javafx.scene.shape.SVGPath
 import system.image.ImageResolver
 
-const val TAB_OVERFLOW = 30.0
+const val TAB_OVERFLOW = 40.0
+const val TAB_INITIAL_OFFSET = -20.0
+
+const val TAB_SELECTOR_HEIGHT = 60.0
 
 class TabSelector(val imageResolver: ImageResolver) : Pane() {
     private val tabs = mutableListOf<Tab>()
 
+    private var selectedTab : Tab? = null
+
     init {
-        tabs.add(Tab(imageResolver, "Devices", "asset:star"))
-        tabs.add(Tab(imageResolver, "Devices1", "asset:star"))
-        tabs.add(Tab(imageResolver, "Devices2", "asset:star"))
-        tabs.add(Tab(imageResolver, "Devices3", "asset:star"))
-        tabs.add(Tab(imageResolver, "Devices4", "asset:star"))
+        tabs.add(Tab(imageResolver, "Devices", "asset:airplay"))
+        tabs.add(Tab(imageResolver, "Layouts", "asset:layout"))
+        tabs.add(Tab(imageResolver, "Commands", "asset:command"))
+        tabs.add(Tab(imageResolver, "Send", "asset:send"))
+        tabs.add(Tab(imageResolver, "Settings", "asset:settings"))
 
         tabs.forEach { tab ->
             this.children.add(tab)
 
-            tab.styleClass.add("tab-selector-tab")
-
             tab.setOnAction {
-                tab.toFront()
+                selectedTab = tab
+
+                renderSelection()
             }
         }
 
-        tabs[0].styleClass.add("tab-left")
-        tabs[tabs.size-1].styleClass.add("tab-right")
+        // Setup the clipping mask for the border
+        val clippingMask = SVGPath()
+        clippingMask.content = "M0,${TAB_SELECTOR_HEIGHT}l0.089-44.04C0.109,8.299,8.425,0,18.625,0h315c4.922,0,9.559,1.93,13.056,5.434c3.497,3.504,5.418,8.145,5.408,13.067l-0.089,44.04L0,${TAB_SELECTOR_HEIGHT}z"
+        clippingMask.layoutX = 0.0
+        clippingMask.layoutY = 0.0
+        this.clip = clippingMask
+
+        this.prefHeight = TAB_SELECTOR_HEIGHT
+
+        // Initially select the first one
+        selectedTab = tabs[0]
+        Platform.runLater {renderSelection()}
+    }
+
+    private fun renderSelection() {
+        // Bring all the other tabs to back
+        tabs.forEach { tab -> tab.toBack() }
+
+        tabs.subList(0, tabs.indexOf(selectedTab)).reversed().forEach { it.toBack() }
+
+        tabs.forEach { tab ->
+            tab.selected = tab == selectedTab
+        }
+
+        selectedTab?.toFront()
     }
 
     override fun layoutChildren() {
@@ -37,7 +69,9 @@ class TabSelector(val imageResolver: ImageResolver) : Pane() {
 
         tabs.forEachIndexed { i, tab ->
             tab.prefWidth = tabWidth
-            tab.layoutX = tabSpacing * i
+            tab.layoutX = TAB_INITIAL_OFFSET + tabSpacing * i
         }
+
+        renderSelection()
     }
 }
