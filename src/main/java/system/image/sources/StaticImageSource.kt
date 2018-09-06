d3bc29a94dbed5af162188d4c09e849762e48e2c
Federico Terzi <freddy6896@gmail.com>
2018-09-06 16:42:17 +0200

    Made all the static images black

--- src/main/java/system/image/sources/StaticImageSource.kt
+++ src/main/java/system/image/sources/StaticImageSource.kt
@@ -9,16 +9,9 @@ import java.io.File
 @RegisterSource(scheme = "static")
 class StaticImageSource(context: ImageSourceContext) : AbstractImageSource(context) {
     override fun resolveFileInternal(id: String): File? {
-        var theme : String = "DARK"  // DARK is the default theme
         var identifier : String = id // Initially, if no theme is specified, the identifier is equal to the id
 
-        // Check if the static image has a theme specified
-        if (id.contains(":")) {  // Images id should be in this format -> "identifier:theme" where theme is optional
-            val tokens = id.split(":")
-            theme = tokens[1]
-            identifier = tokens[0]
-        }
-        val imageFile = ResourceUtils.getResource("/sicons/$theme/$identifier.png");
+        val imageFile = ResourceUtils.getResource("/sicons/$identifier.png")
         if (imageFile != null) {
             return imageFile
         }
