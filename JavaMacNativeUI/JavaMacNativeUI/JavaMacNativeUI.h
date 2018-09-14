47c167aa5a675a8dfa970a227a968c808a9139d8
Federico Terzi <freddy6896@gmail.com>
2018-09-14 09:50:02 +0200

    Add native mac xcode project

---
+++ JavaMacNativeUI/JavaMacNativeUI/JavaMacNativeUI.h
@@ -0,0 +1,13 @@
+//
+//  JavaMacNativeUI.h
+//  JavaMacNativeUI
+//
+//  Created by FreddyT on 14/09/2018.
+//  Copyright © 2018 Federico Terzi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JavaMacNativeUI : NSObject
+
+@end
---
+++ JavaMacNativeUI/JavaMacNativeUI/JavaMacNativeUI.m
@@ -0,0 +1,13 @@
+//
+//  JavaMacNativeUI.m
+//  JavaMacNativeUI
+//
+//  Created by FreddyT on 14/09/2018.
+//  Copyright © 2018 Federico Terzi. All rights reserved.
+//
+
+#import "JavaMacNativeUI.h"
+
+@implementation JavaMacNativeUI
+
+@end
