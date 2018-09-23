7c7dc727adf39ccd80905e1770a027bda886e9fb
Federico Terzi <freddy6896@gmail.com>
2018-09-23 12:17:19 +0200

    Create section if not existing after a open editor request

--- src/main/java/system/server/handlers/RequestEditorHandler.kt
+++ src/main/java/system/server/handlers/RequestEditorHandler.kt
@@ -12,6 +12,17 @@ class RequestEditorHandler(val context: MobileServerContext) : ServiceHandler {
     override fun onServiceRequest(body: JSONObject?): JSONObject? {
         val requestedSectionId = body?.optString("section_id", null)
 
+        // If the section doesn't already exists, create one.
+        if (requestedSectionId != null && context.sectionManager.getSection(requestedSectionId) == null) {
+            if (requestedSectionId.startsWith("app:")) {
+                val requestedApp = requestedSectionId.split("app:")[1]
+                val application = context.applicationManager.getApplication(requestedApp)
+                if (application != null) {
+                    context.sectionManager.createSectionForApp(application)
+                }
+            }
+        }
+
         BroadcastManager.getInstance().sendBroadcast(BroadcastManager.OPEN_EDITOR_REQUEST_EVENT, requestedSectionId)
 
         return null
