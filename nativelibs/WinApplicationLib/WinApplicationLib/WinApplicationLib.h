12a1c7a38710b5cc70a1fef05323ac34c6201e5d
Federico Terzi <freddy6896@gmail.com>
2018-10-23 15:17:00 +0200

    Add dokey prefix to application identifiers

--- nativelibs/WinApplicationLib/WinApplicationLib/WinApplicationLib.h
+++ nativelibs/WinApplicationLib/WinApplicationLib/WinApplicationLib.h
@@ -8,4 +8,9 @@
 
 #define ActiveAppCallback void(*callback)(HWND hwnd, const wchar_t * executablePath, int isUWPApp, const wchar_t * uwpId)
 
+/*
+	Enumerate all the active applications, by invoking the callback for each one found.
+	Note: the callback is invoked only once per application.
+*/
 extern "C" WINAPPLICATIONLIB_API void listActiveApplications(ActiveAppCallback);
+
