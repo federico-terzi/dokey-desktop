220650b22404db3662d5effdafa58d45fe782272
Federico Terzi <freddy6896@gmail.com>
2018-10-23 17:31:26 +0200

    Add focus application native method

--- nativelibs/WinApplicationLib/WinApplicationLib/WinApplicationLib.h
+++ nativelibs/WinApplicationLib/WinApplicationLib/WinApplicationLib.h
@@ -6,11 +6,27 @@
 #define WINAPPLICATIONLIB_API __declspec(dllimport)
 #endif
 
-#define ActiveAppCallback void(*callback)(HWND hwnd, const wchar_t * executablePath, int isUWPApp, const wchar_t * uwpId)
+typedef int(*ActiveAppCallback)(HWND hwnd, const wchar_t * executablePath, int isUWPApp, const wchar_t * appId);
 
 /*
 	Enumerate all the active applications, by invoking the callback for each one found.
 	Note: the callback is invoked only once per application.
 */
-extern "C" WINAPPLICATIONLIB_API void listActiveApplications(ActiveAppCallback);
+extern "C" WINAPPLICATIONLIB_API void listActiveApplications(ActiveAppCallback callback);
 
+
+/*
+	Application Focus
+*/
+
+const long APPLICATION_FOCUS_CHECK_INTERVAL = 300;
+
+/*
+	Attempt to focus the requested application.
+	Return:
+	* 1 if succeeded.
+	* 2 the application was already focused.
+	* -1 if the application was not open.
+	* -2 if the application was open, but could not be focused.
+*/
+extern "C" WINAPPLICATIONLIB_API int focusApplication(const wchar_t * applicationId);
