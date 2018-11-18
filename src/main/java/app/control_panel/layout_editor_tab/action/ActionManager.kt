6995c9113ad1e0dec53bb7e15c05bc17a3e1609d
Federico Terzi <freddy6896@gmail.com>
2018-11-18 08:48:48 +0100

    Fix bug in redo operation

--- src/main/java/app/control_panel/layout_editor_tab/action/ActionManager.kt
+++ src/main/java/app/control_panel/layout_editor_tab/action/ActionManager.kt
@@ -14,6 +14,7 @@ class ActionManager {
     fun execute(action: Action) {
         action.execute()
         undoStack.push(action)
+        redoStack.clear()  // Empty the redo stack
     }
 
     fun undo() {
